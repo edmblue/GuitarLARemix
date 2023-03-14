55fcfe80aa970dc2c206269af22622c629058701
Raquel Betancourt <114664493+edmblue@users.noreply.github.com>
2023-03-14 12:37:52 -0300

    Update nosotros.server.js

--- app/models/nosotros.server.js
+++ app/models/nosotros.server.js
@@ -2,7 +2,7 @@ export async function getNosotrosData() {
   const url = `${process.env.API_URL}/info-nosotros?populate=image`;
   try {
     const response = await fetch(url);
-    return response.json();
+    return await response.json();
   } catch (error) {
     console.log(error);
   }

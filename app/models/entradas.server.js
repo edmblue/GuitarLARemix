f1034a29f32c92c6c28603097e62c0506e6c24e7
Raquel Betancourt <114664493+edmblue@users.noreply.github.com>
2023-03-14 12:37:28 -0300

    Update entradas.server.js

--- app/models/entradas.server.js
+++ app/models/entradas.server.js
@@ -2,7 +2,7 @@ export async function getEntradas() {
   const url = `${process.env.API_URL}/posts?populate=image`;
   try {
     const response = await fetch(url);
-    return response.json();
+    return await response.json();
   } catch (error) {
     console.log(error);
   }
@@ -12,7 +12,7 @@ export async function getEntradaItem(urlItem) {
   const url = `${process.env.API_URL}/posts?populate=image&filters[url]=${urlItem}`;
   try {
     const response = await fetch(url);
-    return response.json();
+    return await response.json();
   } catch (error) {
     console.log(error);
   }
